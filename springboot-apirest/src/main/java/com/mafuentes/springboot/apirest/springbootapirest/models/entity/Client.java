fb96ad1a8942e30495154520730938c77980d2e7
Marcos Fuentes <marcosfuentes691@gmail.com>
2022-11-05 17:45:23 -0300

    Course section 12

--- springboot-apirest/src/main/java/com/mafuentes/springboot/apirest/springbootapirest/models/entity/Client.java
+++ springboot-apirest/src/main/java/com/mafuentes/springboot/apirest/springbootapirest/models/entity/Client.java
@@ -8,10 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -19,11 +19,6 @@ import javax.validation.constraints.Size;
 public class Client implements Serializable {
  
     private static final long serialVersionUID = 1L;
-
-    @PrePersist
-    public void prePersist() {
-        createdAt = new Date();
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,6 +38,7 @@ public class Client implements Serializable {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @NotNull
     @Column(name = "created_at")
     private Date createdAt;
 
